a24f22cd277841f0ad70e53c44c430eb383a9984
Alex Tarazona <alex.tarazona.193@gmail.com>
2025-10-18 10:05:07 -0500

    save

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',    
-  password: '123',
+  password: 'xandertl',
   database: 'tienda',
    port: 3306  
 });
